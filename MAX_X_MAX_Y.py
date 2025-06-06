8f0495a950882da8ad5e188716780dca18eb2727
FE1007 <flo@new-dimension.de>
2025-06-06 15:17:23 +0200

    Update MAX_X_MAX_Y.py

--- MAX_X_MAX_Y.py
+++ MAX_X_MAX_Y.py
@@ -7,13 +7,16 @@ url = 'https://raw.githubusercontent.com/TV354/Hector/refs/heads/main/Dataframes
 df = pd.read_csv(url, index_col=0)
 
 # define variables with the balls min and max coordinates of both X and Y 
-min_x = df['D_X_BotID 6 Y'].min()
-max_x = df['D_X_BotID 6 Y'].max()
-min_y = df['D_Y_BotID 6 Y'].min()
-max_y = df['D_Y_BotID 6 Y'].max()
+label = "4 Y"
+min_x = df[f'D_X_BotID {label}'].min()
+max_x = df[f'D_X_BotID {label}'].max()
+min_y = df[f'D_Y_BotID {label}'].min()
+max_y = df[f'D_Y_BotID {label}'].max()
 
 # display variables 
 print(f"Min X: {min_x}")
 print(f"Max X: {max_x}")
 print(f"Min Y: {min_y}")
 print(f"Max Y: {max_y}")
+
+# list
