34fa0ae1c3df558e21b3e47cd03fcb0ce5d56d43
FE1007 <flo@new-dimension.de>
2025-06-06 15:21:44 +0200

    Update MAX_X_MAX_Y.py

--- MAX_X_MAX_Y.py
+++ MAX_X_MAX_Y.py
@@ -6,17 +6,17 @@ import numpy.random as npr
 url = 'https://raw.githubusercontent.com/TV354/Hector/refs/heads/main/Dataframes/2.csv'
 df = pd.read_csv(url, index_col=0)
 
-# define variables with the balls min and max coordinates of both X and Y 
+# define searched item
 label = "4 Y"
+
+# define variables with the balls min and max coordinates of both X and Y 
 min_x = df[f'D_X_BotID {label}'].min()
 max_x = df[f'D_X_BotID {label}'].max()
 min_y = df[f'D_Y_BotID {label}'].min()
 max_y = df[f'D_Y_BotID {label}'].max()
 
 # display variables 
-print(f"Min X: {min_x}")
-print(f"Max X: {max_x}")
-print(f"Min Y: {min_y}")
-print(f"Max Y: {max_y}")
-
-# list
+print(f"Min X: {min_x:.0f}")
+print(f"Max X: {max_x:.0f}")
+print(f"Min Y: {min_y:.0f}")
+print(f"Max Y: {max_y:.0f}")
